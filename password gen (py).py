45b2035174f94666b1d79808a9eaf87a78e98f6f
pietroperfett <79262604+pietroperfett@users.noreply.github.com>
2021-11-18 18:24:23 +0100

    Update password gen (py).py

--- password gen (py).py
+++ password gen (py).py
@@ -14,9 +14,6 @@ uppercaseLetter4=chr(random.randint(65,90))
 uppercaseLetter5=chr(random.randint(65,90)) 
 uppercaseLetter6=chr(random.randint(65,90)) 
 uppercaseLetter7=chr(random.randint(65,90)) 
-uppercaseLetter8=chr(random.randint(65,90)) 
-uppercaseLetter9=chr(random.randint(65,90)) 
-upperCaseNumber1
 
 # ⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄
 # ⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥ 
@@ -32,11 +29,7 @@ upperCaseNumber1
 # ⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄
 #  ⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠄⠄⠄⠄⠄
 
-# per verifica : https://password.kaspersky.com/it/
-
-password = uppercaseLetter1 + uppercaseLetter2 + uppercaseLetter3 + uppercaseLetter4 + uppercaseLetter5 + uppercaseLetter6 + uppercaseLetter7 + uppercaseLetter8 + uppercaseLetter9 + uppercaseLetter5
-password = shuffle(password + 'p' + ')' + '7' + '25' + '-' + '-')
+password = uppercaseLetter1 + uppercaseLetter2 + uppercaseLetter3 + uppercaseLetter4 + uppercaseLetter5 + uppercaseLetter6 + uppercaseLetter7 
+password = shuffle(password + 'p' + '7' + '25')
 
 print(password)
-
-
